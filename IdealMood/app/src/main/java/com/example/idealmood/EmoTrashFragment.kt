3a2e2b2b389d3ef38a3e2075bf2ed4aab9287384
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2020-06-15 08:31:22 +0900

    recyclerview inflate 완료

--- IdealMood/app/src/main/java/com/example/idealmood/EmoTrashFragment.kt
+++ IdealMood/app/src/main/java/com/example/idealmood/EmoTrashFragment.kt
@@ -20,18 +20,17 @@ class EmoTrashFragment : Fragment() {
     var array = ArrayList<emoTrashData>()
     lateinit var adapter:emoTrashAdapter
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_emo_trash, container, false)
-        init()
-        return view
-    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-    private fun init() {
-        putData()
+
+        //putData()
+        //임시 Data -> filescan으로 수정 예정
+        //emoTrashData title -> emoTrashData contents 의 short ver 분리되어있다고 생각했는데 이것도 수정 필요.
+        array.add(emoTrashData("너무 억울하다..", "20.1.3", "내용1"))
+        array.add(emoTrashData("화가 난다", "20.3.7", "내용2"))
+        array.add(emoTrashData("이해가 안된다", "20.5.17", "내용3"))
+
         emoTrashRecyclerView?.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         adapter = emoTrashAdapter(array)
         adapter.itemClickListener = object:emoTrashAdapter.OnItemClickListener{
@@ -48,11 +47,19 @@ class EmoTrashFragment : Fragment() {
         emoTrashRecyclerView?.adapter = adapter
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_emo_trash, container, false)
+
+
+        return view
+    }
+
+
+
     private fun putData() {
-        //임시 Data -> filescan으로 수정 예정
-        //emoTrashData title -> emoTrashData contents 의 short ver 분리되어있다고 생각했는데 이것도 수정 필요.
-        array.add(emoTrashData("너무 억울하다..", "20.1.3", "내용1"))
-        array.add(emoTrashData("화가 난다", "20.3.7", "내용2"))
-        array.add(emoTrashData("이해가 안된다", "20.5.17", "내용3"))
     }
 }
