93d28ea54934caee3859b65a882b69d934991796
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2020-06-15 10:29:36 +0900

    감정달력 커스텀 완료

---
+++ IdealMood/app/src/main/java/com/example/idealmood/CalendarAdapter.kt
@@ -0,0 +1,79 @@
+package com.example.idealmood
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import java.util.*
+
+class CalendarAdapter(val frag:CalendarFragment) : RecyclerView.Adapter<CalendarAdapter.CalViewHolder>() {
+
+    val baseCalendar = BaseCalendar()
+
+    init {
+        baseCalendar.initBaseCalendar {
+            refreshView(it)
+        }
+    }
+
+    private fun refreshView(calendar: Calendar) {
+        notifyDataSetChanged()
+        frag.refreshCurrentMonth(calendar)
+    }
+
+    interface OnItemClickListener{
+        fun OnItemClick(holder: CalViewHolder, view: View,  position:Int)
+    }
+
+    var itemClickListener : OnItemClickListener?=null
+
+
+    inner class CalViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+        var item_date = containerView.findViewById<TextView>(R.id.item_date)
+
+        init{
+            itemView.setOnClickListener {
+                itemClickListener?.OnItemClick(this, it, adapterPosition)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_day, parent, false)
+        return CalViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return BaseCalendar.LOW_OF_CALENDAR * BaseCalendar.DAYS_OF_WEEK
+    }
+
+    override fun onBindViewHolder(holder: CalViewHolder, position: Int) {
+        if (position % BaseCalendar.DAYS_OF_WEEK == 0) holder.item_date.setTextColor(Color.parseColor("#ff1200"))
+        else holder.item_date.setTextColor(Color.parseColor("#676d6e"))
+
+        if (position < baseCalendar.prevMonthTailOffset || position >= baseCalendar.prevMonthTailOffset + baseCalendar.currentMonthMaxDate) {
+            holder.item_date.alpha = 0.3f
+        } else {
+            holder.item_date.alpha = 1f
+        }
+        holder.item_date.text = baseCalendar.data[position].toString()
+    }
+
+    fun changeToPrevMonth() {
+        baseCalendar.changeToPrevMonth {
+            refreshView(it)
+        }
+    }
+
+    fun changeToNextMonth() {
+        baseCalendar.changeToNextMonth {
+            refreshView(it)
+        }
+    }
+
+}
--- IdealMood/app/src/main/java/com/example/idealmood/CalendarFragment.kt
+++ IdealMood/app/src/main/java/com/example/idealmood/CalendarFragment.kt
@@ -5,11 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_calendar.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class CalendarFragment : Fragment() {
+
+    lateinit var adapter:CalendarAdapter
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = CalendarAdapter(this)
+
+        rcyCalendarView.layoutManager = GridLayoutManager(activity, BaseCalendar.DAYS_OF_WEEK)
+        rcyCalendarView.adapter = adapter
+        rcyCalendarView.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.HORIZONTAL))
+        rcyCalendarView.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+
+        prevMonthBtn.setOnClickListener {
+            adapter.changeToPrevMonth()
+        }
+
+        nextMonthBtn.setOnClickListener {
+            adapter.changeToNextMonth()
+        }
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +46,11 @@ class CalendarFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    fun refreshCurrentMonth(calendar: Calendar) {
+        val sdf = SimpleDateFormat("yyyy MM", Locale.KOREAN)
+        currMonth.text = sdf.format(calendar.time)
     }
 
 }
