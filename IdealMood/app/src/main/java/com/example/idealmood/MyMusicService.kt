688e85cc4cdb8bc479006dc2f245c1841701df6e
HyezNee <46377695+HyezNee@users.noreply.github.com>
2020-11-06 02:46:35 +0900

    솔루션 2개(빗소리, 파도소리) 구현 완료

--- IdealMood/app/src/main/java/com/example/idealmood/MyMusicService.kt
+++ IdealMood/app/src/main/java/com/example/idealmood/MyMusicService.kt
@@ -1,37 +1,102 @@
 package com.example.idealmood
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.media.MediaPlayer
-import android.os.Binder
-import android.os.IBinder
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import java.io.File
+import kotlin.properties.Delegates
 
 class MyMusicService : Service() {
-    lateinit var myPlayer: MediaPlayer
+    var myPlayer :MediaPlayer? = null
+    val screenList = arrayOf<AppCompatActivity>(RainySoundSolution(), WaveSoundSolution())
+    //var mActivityMessenger :Messenger ?= null
 
-    public class LocalBinder : Binder() {
-        public fun getService() : MyMusicService {
-            return MyMusicService()
-        }
+    /*companion object {
+        const val PLAYING = 0
+        const val STOPING = 1
     }
-    private var mBinder = LocalBinder()
+
+    class ServiceHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            if(msg.what == PLAYING) {
+                myPlayer.stop()
+                myPlayer.release()
+            }
+        }
+    }*/
+
+    class LocalBinder : Binder() {
+            fun getService(): MyMusicService {
+                return MyMusicService() // 현재 서비스 반환
+            }
+    }
+
+    companion object {
+        private val mBinder :IBinder = LocalBinder() // 객체 만들기
+    }
 
     override fun onBind(intent: Intent): IBinder {
-        return mBinder
+        return mBinder  // onBind함수의 return값으로 사용
     }
 
-    //private var mConnection =
+    /*fun getIsPlay() :Boolean{
+        return myPlayer.isPlaying
+    }*/
+
+
+    /*private var mConnection :ServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            //TODO("Not yet implemented")
+        }
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            //TODO("Not yet implemented")
+        }
+    }*/
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        var isPlay: Boolean? = intent?.extras?.getBoolean("isPlay")
-        if(isPlay!!) {
-            myPlayer = MediaPlayer.create(this, R.raw.rainysound)
-            myPlayer.start()
+        var playIdx = intent?.extras?.getInt("playIdx")
+        var music = when (playIdx){
+            0 -> R.raw.rainysound
+            1 -> R.raw.oceanwavesound
+            else -> null
         }
-        else {
-            myPlayer.stop()
-            myPlayer.release()
+
+        val mNotifyMgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(myPlayer == null) {  // Play 버튼 눌렀을 때
+            myPlayer = MediaPlayer.create(this, music!!)
+            myPlayer!!.start()
+
+            val msgIntent = Intent(this, screenList[playIdx!!]::class.java)
+            val msgPendingIntent = PendingIntent.getActivity(
+                this, 1, msgIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            // API 26버전부터 추가
+            val mchannel = NotificationChannel("1", "rainySound", NotificationManager.IMPORTANCE_DEFAULT)
+            mNotifyMgr.createNotificationChannel(mchannel)
+
+            val mBuilder =  NotificationCompat.Builder(this, "1")
+            with(mBuilder) {
+                setSmallIcon(R.drawable.ic_favorite_white_28dp)
+                setContentTitle("솔루션 재생 중")
+                setContentIntent(msgPendingIntent)
+                setContentText("탭하여 앱 실행하기")
+            }
+            mNotifyMgr.notify(1, mBuilder.build())  // id는 나중에 알림 제거할 때 사용
+        }
+        else {  // stop 버튼 눌렀을 때
+            myPlayer!!.stop()
+            myPlayer!!.release()
+            stopService(intent)
+            mNotifyMgr.cancel(1)
         }
 
         //return super.onStartCommand(intent, flags, startId)
--- IdealMood/app/src/main/java/com/example/idealmood/RainySoundSolution.kt
+++ IdealMood/app/src/main/java/com/example/idealmood/RainySoundSolution.kt
@@ -1,12 +1,19 @@
 package com.example.idealmood
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.idealmood.MyMusicService.LocalBinder
 import kotlinx.android.synthetic.main.activity_rainy_sound_solution.*
 
 class RainySoundSolution : AppCompatActivity() {
-    var isPlay :Boolean = false
+    var isPlay :Boolean = true
+    private var mService :MyMusicService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,19 +21,33 @@ class RainySoundSolution : AppCompatActivity() {
         init()
     }
 
+    /*override fun onResume() {
+        super.onResume()
+        if(mService != null && isPlaying!!)
+            firstPlay = false
+        Log.i("서비스 : ", "$mService / $isPlaying")
+    }*/
+
+    /*private val mConnection :ServiceConnection = object :ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as MyMusicService.LocalBinder
+            mService = binder.getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mService = null
+        }
+    }*/
+
+    // 서비스 실행 시 서비스에 있는 함수 호출
+    //var isPlaying = mService?.getIsPlay()
+
     private fun init() {
-        playBtn.setOnClickListener {
+        rain_playBtn.setOnClickListener {
             val intent = Intent(this, MyMusicService::class.java)
-            if (isPlay == false) {  // "PLAY" 버튼을 눌렀을 때
-                isPlay = true
-                playBtn.text = "STOP"
-                intent.putExtra("isPlay", true)
-            }
-            else {  // "STOP" 버튼을 눌렀을 때
-                isPlay = false
-                playBtn.text = "PLAY"
-                intent.putExtra("isPlay", false)
-            }
+            intent.putExtra("playIdx", 0)
+            //bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+            startService(intent)
         }
     }
 }
