0033b001bfcbbdfd02f39ec9ccd545338705cbdc
HyezNee <46377695+HyezNee@users.noreply.github.com>
2020-11-06 16:48:07 +0900

    용량 줄이기

--- IdealMood/app/src/main/java/com/example/idealmood/MeditationSolution.kt
+++ IdealMood/app/src/main/java/com/example/idealmood/MeditationSolution.kt
@@ -1,12 +1,36 @@
 package com.example.idealmood
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_meditation_solution.*
 
 class MeditationSolution : AppCompatActivity() {
+
+    lateinit var mediPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meditation_solution)
+        init()
+    }
+
+    private fun init() {
+        medi_backBtn.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // 해당 액티비티가 실행되면 음악이 자동으로 재생
+        mediPlayer = MediaPlayer.create(this, R.raw.meditationsound)
+        mediPlayer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediPlayer.stop()
+        mediPlayer.release()
     }
 }
